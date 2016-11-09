1adebfc732bc057acae557e6b15d043373a11cb3
Teemu Y <theme87@gmail.com>
2016-11-09 04:07:09 +0200

    added functioning participation buttons

--- app/controllers/participations_controller.rb
+++ app/controllers/participations_controller.rb
@@ -1,16 +1,25 @@
-class EventsController < ApplicationController
+class ParticipationsController < ApplicationController
   def create
+    if current_user.event_participations.ids.include?(params[:event_id].to_i)
+      destroy_old_participation(current_user
+        .participations
+        .where(event_id: params[:event_id]))
+    end
     participation = Participation.new(participation_params)
     participation.user = current_user
-
     if participation.save
-      redirect_back(fallback_location: root_path), notice: 'saved'
+      redirect_back(fallback_location: root_path)
     else
-      redirect_back(fallback_location: root_path), notice: 'Was not able to save'
+      redirect_back(fallback_location: root_path)
+    end
   end
 
   private
   def participation_params
-    params.require(:participation).permit(:event, :status)
+    params.permit(:event_id, :status)
+  end
+
+  def destroy_old_participation(participation_id)
+    Participation.destroy(participation_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :locations, except: [:destroy, :edit, :update]
   root to: 'events#index'
   resources :events
+  resources :participations, only: [:create]
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
